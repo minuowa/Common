24c353d34a8ad54103bb2945e0a5c1a43686350c
minuowa <939813822@qq.com>
2014-05-04 16:48:12 +0800

    Signed-off-by: minuowa <939813822@qq.com>

---
+++ XBase.h
@@ -0,0 +1,20 @@
+#ifndef CXBase_h__
+#define CXBase_h__
+//--------------------------------------------------------------------------------------------------
+#include "base.h"
+#include "XSingleton.h"
+#include "XString.h"
+#include "XMap.h"
+#include "XMultiMap.h"
+#include "XList.h"
+#include "XDynaArray.h"
+#include "XStaticArray.h"
+#include "XBuffer.h"
+#include "XBiTree.h"
+#include "XRapidxmlLoader.h"
+#include "XRapidxmlWriter.h"
+#include "XIDGenerator.h"
+#include "XPath.h"
+#include "XMath.h"
+//--------------------------------------------------------------------------------------------------
+#endif // CXBase_h__
